45cc4787d7dea537eb313e617641e0c47d26cbe0
berkbakkaloglu <berkbakkaloglu@gmail.com>
2024-10-07 18:58:22 +0300

    fixed bugs

--- index.jsx
+++ index.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client" ;
 import { BrowserRouter , Routes , Route , Link} from "react-router-dom"
-import Layout from "../Portfolio/components/Layout";
+import Layout from "./components/Layout";
 import Home from "./pages/Home"
 import ProjectDetail from "./pages/Projects/ProjectDetails";
 import Projects from "./pages/Projects/Projects";
