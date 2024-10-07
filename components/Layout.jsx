241280ec1862971da4df55ae4f9d761bad01c2fc
berkbakkaloglu <berkbakkaloglu@gmail.com>
2024-10-07 18:41:14 +0300

    fixed bugs

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom"
 import Header from "./Header"
 import Footer from "./Footer"
 
-export default function() {
+export default function Layout() {
     return (
         <>
             <Header />
